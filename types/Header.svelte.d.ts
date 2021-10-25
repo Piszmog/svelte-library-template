0a56d87a81da7d2f690f5ee7bf395898dc17dfde
Piszmog <piszmogcode@gmail.com>
2021-10-25 16:08:37 -0600

    Add types

---
+++ types/Header.svelte.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="svelte" />
+import { SvelteComponentTyped } from "svelte";
+
+export interface HeaderProps {
+  value?: undefined;
+}
+
+export default class Header extends SvelteComponentTyped<HeaderProps, {}, {}> {}
